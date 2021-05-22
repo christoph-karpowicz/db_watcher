6379399761d107de82411360483ee869b7dbbaf0
christoph-karpowicz <christoph.karpowicz@gmail.com>
2021-05-22 09:37:07 +0200

    Update scripts and examples.

--- dbw.sh
+++ dbw.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-if [ $1 = "test-orcl" ]; then
-    mvn clean test -DtestConfigPath="./config/orcl-example.yml"
-elif [ $1 = "test-postgres" ]; then
-    mvn clean test -DtestConfigPath="./config/postgres-example.yml"
-elif [ $1 = "compile" ]; then
-    mvn package shade:shade -DskipTests=true
-else
-    java -Xmx25m -cp target/dbw-1.1.0.jar com.dbw.app.Dbw "$@"
-fi
+java -Xmx25m -cp target/dbw-1.1.0.jar com.dbw.app.Dbw "$@"
